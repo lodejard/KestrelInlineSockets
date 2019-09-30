04a468f5a54bf931ea2a34aef4088021c517f9cd
Louis DeJardin <lodejard@microsoft.com>
2019-09-30 13:06:32 -0700

    Enable application to schedule inline (#14)
    Transport layer shouldn't need to implement worker thread
    hop in order to protect accept loop

--- examples/HelloWorld/Program.cs
+++ examples/HelloWorld/Program.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Transport.Abstractions.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -20,8 +21,14 @@ namespace HelloWorld
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureKestrel(options =>
+                {
+                    options.ApplicationSchedulingMode = SchedulingMode.Inline;
+                });
+        }
     }
 }
